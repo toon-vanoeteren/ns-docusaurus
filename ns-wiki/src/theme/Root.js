9b0bbc9e41038e5410ccc1e229977b0019071f8b
ToonVanoeteren <toon.vanoeteren@cegeka.com>
2025-11-24 16:36:51 +0100

    ai2

--- ns-wiki/src/theme/Root.js
+++ ns-wiki/src/theme/Root.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Root({ children }) {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'http://20.199.85.40:3001/embed/anythingllm-chat-widget.min.js';
+    script.async = true;
+    script.setAttribute('data-embed-id', 'cd4cb257-a5c4-4fff-8bc6-0010f39a3a6f');
+    script.setAttribute('data-base-api-url', 'http://20.199.85.40:3001/api/embed');
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
       {children}
-      <anythingllm-chat workspace="Docusaurus"></anythingllm-chat>
+      <anythingllm-chat></anythingllm-chat>
     </>
   );
 }
