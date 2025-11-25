9eb4d127bb528d9789e06c3c0277a512a701135f
ToonVanoeteren <toon.vanoeteren@cegeka.com>
2025-11-25 16:28:56 +0100

    openAI hook

--- ns-wiki/src/theme/Root.js
+++ ns-wiki/src/theme/Root.js
@@ -3,10 +3,10 @@ import React, { useEffect } from 'react';
 export default function Root({ children }) {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'http://20.199.85.40:3001/embed/anythingllm-chat-widget.min.js';
+    script.src = 'http://172.16.0.5:3001/embed/anythingllm-chat-widget.min.js';
     script.async = true;
-    script.setAttribute('data-embed-id', 'baa939c7-3bd9-497b-83dc-297ba189c859');
-    script.setAttribute('data-base-api-url', 'http://20.199.85.40:3001/api/embed');
+    script.setAttribute('data-embed-id', '85284776-c5d3-46af-a1f2-e6e7e5da8c3e');
+    script.setAttribute('data-base-api-url', 'http://172.16.0.5:3001/api/embed');
     document.body.appendChild(script);
   }, []);
 
